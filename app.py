55615c805479cb4f03e68513ff3cc2f96cd1c05b
Hussain Nazeer Latiff <HussainLatiff@users.noreply.huggingface.co>
2023-12-06 05:17:22 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,18 @@
 import gradio as gr
-import numpy as np
+import os
 
-# Import any additional libraries needed for your video processing task
-import cv2
 
-def process_video(video_path):
-    # Read the video file
-    video = cv2.VideoCapture(video_path)
+def video_identity(video):
+    return video
 
-    # Perform any necessary processing on the video
-    # For example, you could extract frames, apply filters, or perform object detection
 
-    # Save the processed video
-    processed_video_path = "processed_video.mp4"
-    cv2.VideoWriter(processed_video_path, fourcc='mp4v', fps=30, frameSize=(640, 480)).write(video)
+demo = gr.Interface(video_identity, 
+                    gr.Video(), 
+                    "playable_video", 
+                    examples=[
+                        os.path.join(os.path.dirname(__file__), 
+                                     "video/video_sample.mp4")], 
+                    cache_examples=True)
 
-    # Return the processed video path
-    return processed_video_path
-
-with gr.Blocks() as f:
-    # Create an input component for uploading videos
-    uploaded_video = gr.Video(label="Upload Video", sources=["upload"])
-
-    # Create a button to trigger the video processing function
-    process_button = gr.Button("Process Video")
-
-    # Create an output component for displaying the processed video
-    processed_video = gr.Video(label="Processed Video")
-
-    # Connect the components
-    processed_video = process_button.click(process_video, inputs=[uploaded_video])
-
-    # Display the interface
-    f.launch()
+if __name__ == "__main__":
+    demo.launch()
