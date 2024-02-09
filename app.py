8c45656b32625bc5ee5bc6a32c78a20a1dfd4fbd
Hussain Nazeer Latiff <HussainLatiff@users.noreply.huggingface.co>
2024-02-09 16:08:46 +0000

    Added instructions that appear

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
 import gradio as gr
 import os
 
+
 def video_identity(video):    
     return video
 
 
-demo = gr.Interface(video_identity,                     
+with gr.Blocks() as demo:
+    gr.Markdown(
+    """
+    # Phone brr
+    Welcome to the Hugging face Space of Phone brr we aim to create more immersive content for mobile phones with the use of haptic audio, this demo focuses on working for a very commonly used special effect of explosions hope you enjoy it.
+
+    Instructions
+     
+    Step 1: Upload the example video to get the relevant timeframes that require haptics <a href="https://portal.vision.cognitive.azure.com/demo/video-summary-and-frame-locator">Azure Cognitive Services Video Summary and Frame Locator</a> with explosions as the query.
+
+    Step 2: Download the generated audio from <a href="https://phonebrrdemonstration2.blob.core.windows.net/audio3second0001/3_second_explosion_00001.flac">this ai-generated haptic audio</a>.
+
+    Step 3: Mix the Audio using any app of your choice and master the audio with <a href="https://aimastering.com/">ai-mastering program</a> 
+
+    """),
+    gr.Interface(video_identity,                     
                     gr.Video(),                     
                     "playable_video",                     
                     examples=[
@@ -15,3 +31,5 @@ demo = gr.Interface(video_identity,
 
 if __name__ == "__main__":    
     demo.launch(share=True)
+
+
