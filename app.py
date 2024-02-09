014ee66411fe412b57455c69aecc89e19c3181a2
Hussain Nazeer Latiff <HussainLatiff@users.noreply.huggingface.co>
2024-02-09 13:40:01 +0000

    Added instructions to perform relavant changes

--- app.py
+++ app.py
@@ -4,6 +4,13 @@ import os
 def video_identity(video):    
     return video
 
+instructions = """
+<b>Instructions:</b><br>
+Step 1: Upload the example video to get the relevant timeframes that require haptics, the text query should be 'explosion'  <a href="https://portal.vision.cognitive.azure.com/demo/video-summary-and-frame-locator">Azure Cognitive Services Video Summary and Frame Locator</a> with explosions as the query.<br>
+Step 2: Download the generated audio from <a href="https://phonebrrdemonstration2.blob.core.windows.net/audio3second0001/3_second_explosion_00001.flac">this ai-generated haptic audio</a>.
+Step 3: Mix the Audio using any app of your choice and master the audio with <a href="https://aimastering.com/">ai-mastering program</a> 
+"""
+
 demo = gr.Interface(video_identity,                     
                     gr.Video(),                     
                     "playable_video",                     
@@ -12,4 +19,4 @@ demo = gr.Interface(video_identity,
                     cache_examples=True)
 
 if __name__ == "__main__":    
-    demo.launch()
+    demo.launch(share=True)
